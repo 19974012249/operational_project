e684d6e475016b12a84f432cf1d98aeeda48a919
dengtong <zhouwei2@szcomtop.com>
2022-08-29 15:20:06 +0800

    修改牛客封装类ListNode、TreeNode源码

--- src/main/java/com/zw/algorithm/common/ListNode.java
+++ src/main/java/com/zw/algorithm/common/ListNode.java
@@ -1,5 +1,7 @@
 package com.zw.algorithm.common;
 
+import java.util.ArrayList;
+
 /**
  * @author zhouwei2
  * @version 1.0.0
@@ -10,23 +12,99 @@ package com.zw.algorithm.common;
 //类名 ：Java类就是一种自定义的数据结构
 public class ListNode {
 
-    //成员变量：数值
-   public int val;
+    public int val;
 
-    //对象 ：引用下一个节点对象。在Java中没有指针的概念，Java中的引用和C语言的指针类似
-    public  ListNode next;
+    public ListNode next;
 
-    //一个参数的构造方法
-    public  ListNode(int val) {
-        //把接收的参数赋值给当前类的val变量
-        this.val = val;
+    public ListNode(int x) {
+
+        val = x;
+        next = null;
     }
 
-    //这个就是包含两个参数的构造方法
-    public ListNode(int val, ListNode next) {
+    //创建单链表
+    public static ListNode makeNode(int[] nums) {
 
-        this.val = val;
-        this.next = next;
+        if (nums.length == 0) {
+            return null;
+        }
+        ListNode listNode = new ListNode(nums[0]);
+        ListNode head = listNode;
+        for (int i = 1; i < nums.length; i++) {
+            ListNode node = new ListNode(nums[i]);
+            listNode.next = node;
+            listNode = node;
+        }
+        return head;
+    }
+
+    //创建循环链表
+    public static ListNode makeNode(int[] nums, int pos) {
+
+        if (nums.length == 0) {
+            return null;
+        }
+        ListNode[] listNodes = new ListNode[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            listNodes[i] = new ListNode(nums[i]);
+        }
+
+        ListNode listNode = listNodes[0];
+        for (int i = 1; i < listNodes.length; i++) {
+            listNode.next = listNodes[i];
+            listNode = listNodes[i];
+        }
+        if (pos >= 0 && pos < nums.length) {
+            listNode.next = listNodes[pos];
+        }
+        return listNodes[0];
+    }
+
+    //创建两条相交链表
+    public static ListNode[] makeIntersectNode(int[] listA, int skipA, int[] listB, int skipB) {
+
+        if (listA.length == 0 || listB.length == 0) {
+            return null;
+        }
+        ListNode[] nodesA = new ListNode[listA.length];
+        for (int i = 0; i < nodesA.length; i++) {
+            nodesA[i] = new ListNode(listA[i]);
+        }
+        ListNode nodeA = nodesA[0];
+        for (int i = 1; i < nodesA.length; i++) {
+            nodeA.next = nodesA[i];
+            nodeA = nodesA[i];
+        }
+
+        ListNode[] nodesB = new ListNode[listB.length];
+        for (int i = 0; i < nodesB.length; i++) {
+            nodesB[i] = new ListNode(listB[i]);
+        }
+        ListNode nodeB = nodesB[0];
+        for (int i = 1; i < nodesB.length; i++) {
+            nodeB.next = nodesB[i];
+            nodeB = nodesB[i];
+        }
+
+        if (skipA < listA.length && skipB < listB.length && listA[skipA] == listB[skipB]) {
+            nodesB[skipB].next = nodesA[skipA];
+        }
+
+        ListNode[] nodes = new ListNode[2];
+        nodes[0] = nodesA[0];
+        nodes[1] = nodesB[0];
+        return nodes;
+    }
+
+    //遍历链表
+    public static ArrayList<Integer> traverse(ListNode head) {
+
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        while (head != null) {
+            arrayList.add(head.val);
+            head = head.next;
+        }
+        return arrayList;
     }
 }
 
